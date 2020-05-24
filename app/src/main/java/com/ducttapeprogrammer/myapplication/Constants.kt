24a0e3daef7b19ecefa56eb0860854c77b6914d5
clintpaul <clintpaul89@gmail.com>
2020-05-24 22:43:04 +0530

    Working on Forecast Fragment: Working on Forecast Fragment UI improvements Added unit test for finding the weather ID provided the weather condition ID

---
+++ app/src/main/java/com/ducttapeprogrammer/myapplication/Constants.kt
@@ -0,0 +1,22 @@
+package com.ducttapeprogrammer.myapplication
+
+//Weather condition
+const val THUNDER_STORM = 100
+const val DRIZZLE = 101
+const val RAIN = 103
+const val SNOW = 104
+const val ATMOSPHERE = 105
+const val CLEAR = 106
+const val CLOUDS = 107
+
+//Weather condition range
+val THUNDER_STORM_RANGE = 200..232
+val DRIZZLE_RANGE = 300..321
+val RAIN_RANGE = 500..531
+val SNOW_RANGE = 600..622
+val ATMOSPHERE_RANGE = 701..781
+val CLEAR_RANGE = 800..800
+val CLOUD_RANGE = 801..804
+
+//SharedPref Keys
+const val SHARED_PREF_WEATHER_CONDITION_KEY = "weather_condition"
--- app/src/main/java/com/ducttapeprogrammer/myapplication/forecast/ForecastFragment.kt
+++ app/src/main/java/com/ducttapeprogrammer/myapplication/forecast/ForecastFragment.kt
@@ -1,13 +1,17 @@
 package com.ducttapeprogrammer.myapplication.forecast
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.ducttapeprogrammer.myapplication.SNOW
 import com.ducttapeprogrammer.myapplication.databinding.FragmentForecastBinding
+import com.ducttapeprogrammer.myapplication.getWeatherConditionIcon
 
-
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class ForecastFragment : Fragment() {
     private lateinit var binding: FragmentForecastBinding
     override fun onCreateView(
@@ -21,6 +25,12 @@ class ForecastFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setWeatherConditionIcon()
 
+    }
+
+    private fun setWeatherConditionIcon() {
+
+        binding.imageViewWeatherCondition.getWeatherConditionIcon(SNOW)
     }
 }
